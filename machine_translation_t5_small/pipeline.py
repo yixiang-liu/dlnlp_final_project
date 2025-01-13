eb966416482038f1716e1155676df21271e35adf
yixiang-liu <2200010875@stu.pku.edu.cn>
2025-01-13 13:54:26 +0800

    Add files via upload

---
+++ machine_translation_t5_small/pipeline.py
@@ -0,0 +1,21 @@
+import warnings 
+warnings.filterwarnings('ignore')
+from transformers import pipeline, AutoTokenizer
+
+
+# 在这里输入英语
+source_language = 'Family, who gets it? Trained an AI model, and the accuracy ended up worse than random guessing!'
+# 在这里输入目标语言种类，目前支持France, Hungarian
+checkpoint = 'machine_translation_t5_small\\trained_t5-small'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+print('translator preparing!')
+translator = pipeline('translation',model=checkpoint,tokenizer=tokenizer,max_length=32)
+print('translator prepared!')
+english_text = f"Translate English into France:{source_language}"
+
+# 进行翻译
+print('translation starts!')
+translated_text = translator(english_text)
+
+# 打印翻译结果
+print("Translated Text:", translated_text[0]['translation_text'])
